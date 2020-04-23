47e4be4aac314c1f999308f404b6b9104d730a02
Prince B. Rodrigues <prince.rodrigues086@gmail.com>
2020-04-23 14:31:26 -0300

    O Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Projeto Git/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Projeto_Git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
