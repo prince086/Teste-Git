d2e29433250887796ec1a0b236e491778b91ef34
Prince B. Rodrigues <prince.rodrigues086@gmail.com>
2020-04-23 14:59:14 -0300

    Acrescentanda a pergunta sobre como voce esta hoje

--- Projeto Git/Program.cs
+++ Projeto Git/Program.cs
@@ -5,6 +5,7 @@ namespace Projeto_Git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.WriteLine();
         }
